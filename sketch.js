6959c3fc4a6d16d398a65e90c016871875e24605
Sergio Granada-Moreno <sergiogranadamoreno@gmail.com>
2020-01-29 20:47:53 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let mic, fft;
+
+function setup() {
+  createCanvas(350, 200);
+  noFill();
+
+  mic = new p5.AudioIn();
+  mic.start();
+  fft = new p5.FFT();
+  fft.setInput(mic);
+}
+
+function draw() {
+  background(200);
+
+  let spectrum = fft.analyze();
+
+  beginShape();
+  for (i = 0; i < spectrum.length; i++) {
+    vertex(i, map(spectrum[i], 0, 255, height, 0));
+  }
+  endShape();
+}
